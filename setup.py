f5aba1d5c264bf74fb92ca7191ea17fbb050f169
Pierre Francois RIGODIAT <pierre-francois.rigodiat@nist.gov>
2017-10-02 11:27:59 -0400

    Update version: alpha4
    Change-Id: Ica884574b4acf4355c619bbaa7048ae3a782ee11

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="core_oaipmh_harvester_app",
-    version="1.0.0-alpha3",
+    version="1.0.0-alpha4",
     description=("core oai-pmh harvester Django package",),
     author="NIST IT Lab",
     author_email="itl_inquiries@nist.gov",
