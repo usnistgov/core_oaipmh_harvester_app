3e2edd07021d9c7174af1d019e769af0ac5a88ca
Xavier SCHMITT <xavier.schmitt@nist.gov>
2019-03-18 11:42:32 -0400

    chore: upgrade 1.1.0
    Change-Id: I464beab05f2a83dba4b5551e363b1866cf24870c

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_oaipmh_harvester_app",
-    version="1.0.1",
+    version="1.1.0",
     description="OAI-PMH harvesting capabilities for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
