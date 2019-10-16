dda261ce04f1a0250b6ba978d43e9bc76f0b2d54
Augustin CHINI <augustin.chini@nist.gov>
2019-10-16 13:59:46 -0400

    chore: upgrade 1.6.0
    Change-Id: Ifb14e430748b752c0259fd023258d60b4a0da38a

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_oaipmh_harvester_app",
-    version="1.5.0",
+    version="1.6.0",
     description="OAI-PMH harvesting capabilities for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
