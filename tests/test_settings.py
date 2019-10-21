2bac46bbadd1bf88c032ee80723bb0f5cc559300
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-21 15:33:29 -0400

    fix: test settings for django 2.2
    Change-Id: Ic2567ba48f35c0b622769ded9befa7a718581204

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -9,11 +9,40 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.sites',
+
     # Local apps
     "core_main_app",
     "tests",
 ]
+
+MIDDLEWARE = (
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+)
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            'templates'
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
+            ],
+        },
+    },
+]
+
 
 OAI_HARVESTER_ROOT = dirname(realpath(__file__))
 
