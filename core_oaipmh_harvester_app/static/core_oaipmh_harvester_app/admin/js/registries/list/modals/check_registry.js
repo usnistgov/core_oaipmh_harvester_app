ac5377283dc87346a2fba39ddc39a2e8d63f1abe
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2021-04-14 22:18:47 -0400

    fix: use fas prefix instead of fa (deprecated)
    Change-Id: I9a76e1178dce555d2f447564accfbb5273dc9a22

--- core_oaipmh_harvester_app/static/core_oaipmh_harvester_app/admin/js/registries/list/modals/check_registry.js
+++ core_oaipmh_harvester_app/static/core_oaipmh_harvester_app/admin/js/registries/list/modals/check_registry.js
@@ -9,7 +9,7 @@ checkStatus = function(event)
     var $registryRow = $(this).parent().parent();
     var url = $registryRow.attr("url");
 
-    $registryTd.html('<i class="fa fa-spinner fa-spin"></i>');
+    $registryTd.html('<i class="fas fa-spinner fa-spin"></i>');
 
     $.ajax({
         url : checkRegistryGetUrl,
@@ -22,16 +22,16 @@ checkStatus = function(event)
         success: function(data){
             if (data.is_available)
             {
-                $registryTd.html('<i class="fa fa-signal"></i> Available');
+                $registryTd.html('<i class="fas fa-signal"></i> Available');
                 $registryTd.css("color", "#5cb85c");
             }
             else {
-                $registryTd.html('<i class="fa fa-signal"></i> Unavailable');
+                $registryTd.html('<i class="fas fa-signal"></i> Unavailable');
                 $registryTd.css("color", "#d9534f");
             }
         },
         error:function(data){
-            $registryTd.html('<i class="fa fa-warning"></i> Error while checking');
+            $registryTd.html('<i class="fas fa-warning"></i> Error while checking');
             $registryTd.css("color", "#d9534f");
         },
     });
