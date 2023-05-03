45c259f4ff5b42a535ef4ef2e6e1f32c85576ed4
Guillaume Sousa <guillaume.sousa@nist.gov>
2023-05-03 02:06:42 +0000

    chore: upgrade 2.4.0
    Change-Id: I46fb95af0daa4a08efd2c7b069df9ddc5753fbe8

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ chdir(normpath(join(abspath(__file__), pardir)))
 
 setup(
     name="core_oaipmh_harvester_app",
-    version="2.3.0",
+    version="2.4.0",
     description="OAI-PMH harvesting capabilities for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
